a45a2f8c6a4fddcd74d7b9a15bc932efd3bbe844
Evgeniy Putilin <mr.putilinz@gmail.com>
2022-11-18 22:25:53 +0300

    second commit

---
+++ src/pattern/singleton/Filter.java
@@ -0,0 +1,28 @@
+package pattern.singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+    protected int treshold;
+
+    public Filter(int treshold) {
+        this.treshold = treshold;
+    }
+
+    public List<Integer> filterOut(List<Integer> source, Logger logger) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < source.size(); i++) {
+            if (source.get(i) < treshold) {
+                logger.log("Элемент " + source.get(i) + " не проходит");
+            } else {
+                result.add(source.get(i));
+                logger.log("Элемент " + source.get(i) + " проходит");
+            }
+        }
+        logger.log("Выводим результат на экран");
+
+        return result;
+    }
+
+}
